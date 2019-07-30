5aba4d3f513b506bec565b997907e6f721b66cd4
lipengcheng3 <lipengcheng3@jd.com>
2019-07-30 21:47:27 +0800

    add: 回顾jdbc的基本用法

---
+++ learn-jdbc/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+/**
+ * Package: PACKAGE_NAME
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2019-07-30
+ * Time: 21:06
+ * Description:
+ */
+public class Main {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException {
+        // 在 com.mysql.cj.jdbc.Driver 类的静态代码块中进行了DriverManager的注册。
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://104.248.236.89:3306/learn","lpc_test","12345aA)");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from form");
+        while (resultSet.next()){
+            String x= resultSet.getString(1);
+            System.out.println("x="+x);
+        }
+//        statement.executeUpdate();
+    }
+}
---
+++ learn-jdbc/src/main/java/MainWithDataSource.java
@@ -0,0 +1,32 @@
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+
+import javax.sql.DataSource;
+import javax.sql.PooledConnection;
+import java.sql.*;
+
+/**
+ * Package: PACKAGE_NAME
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2019-07-30
+ * Time: 21:27
+ * Description:
+ */
+public class MainWithDataSource {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException {
+        // 在 com.mysql.cj.jdbc.Driver 类的静态代码块中进行了DriverManager的注册。
+        MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
+        ds.setUser("lpc_test");
+        ds.setPassword("12345aA)");
+        ds.setUrl("jdbc:mysql://104.248.236.89:3306/learn");
+        // 连接数之类的再看
+
+        PooledConnection p = ds.getPooledConnection();
+        Statement statement = p.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from form");
+        while (resultSet.next()){
+            String x= resultSet.getString(1);
+            System.out.println("x="+x);
+        }
+    }
+}
