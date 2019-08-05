63dc678fa4cb9cce7b4db2b88121d233fc5f451e
lipengcheng3 <lipengcheng3@jd.com>
2019-08-05 18:07:32 +0800

    modify: 修改DataSource的相关实现

--- learn-jdbc/src/main/java/MainWithDataSource.java
+++ learn-jdbc/src/main/java/MainWithDataSource.java
@@ -1,7 +1,9 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
 
 import javax.sql.DataSource;
 import javax.sql.PooledConnection;
+import java.beans.PropertyVetoException;
 import java.sql.*;
 
 /**
@@ -12,16 +14,19 @@ import java.sql.*;
  * Description:
  */
 public class MainWithDataSource {
-    public static void main(String args[]) throws ClassNotFoundException, SQLException {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException, PropertyVetoException {
         // 在 com.mysql.cj.jdbc.Driver 类的静态代码块中进行了DriverManager的注册。
-        MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
+        ComboPooledDataSource ds = new ComboPooledDataSource();
         ds.setUser("lpc_test");
         ds.setPassword("12345aA)");
-        ds.setUrl("jdbc:mysql://104.248.236.89:3306/learn");
-        // 连接数之类的再看
+        ds.setDriverClass("com.mysql.cj.jdbc.Driver");
+        ds.setJdbcUrl("jdbc:mysql://104.248.236.89:3306/learn");
+        ds.setInitialPoolSize(3);
+        ds.setMaxPoolSize(10);
+        ds.setMaxStatements(100);
+        ds.setAcquireIncrement(2);
 
-        PooledConnection p = ds.getPooledConnection();
-        Statement statement = p.getConnection().createStatement();
+        Statement statement = ds.getConnection().createStatement();
         ResultSet resultSet = statement.executeQuery("select * from form");
         while (resultSet.next()){
             String x= resultSet.getString(1);
