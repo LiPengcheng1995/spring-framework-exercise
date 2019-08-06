d11aca9f45247dc0ee85ab18a7deb04cb6243a34
lipengcheng3 <lipengcheng3@jd.com>
2019-08-06 10:09:54 +0800

    add: 增加一些jdbc的使用例子

--- learn-jdbc/src/main/java/MainWithPreparedStatement.java
+++ learn-jdbc/src/main/java/MainWithPreparedStatement.java
@@ -3,17 +3,18 @@ import java.sql.*;
 /**
  * Package: PACKAGE_NAME
  * User: 李鹏程
- * Date: 2019-07-30
- * Time: 21:06
+ * Email: lipengcheng3@jd.com
+ * Date: 2019-08-05
+ * Time: 18:07
  * Description:
  */
-public class Main {
+public class MainWithPreparedStatement {
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
         // 在 com.mysql.cj.jdbc.Driver 类的静态代码块中进行了DriverManager的注册。
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://104.248.236.89:3306/learn","lpc_test","12345aA)");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from form");
+        PreparedStatement statement = connection.prepareStatement("select * from form");
+        ResultSet resultSet = statement.executeQuery();
         while (resultSet.next()){
             String x= resultSet.getString(1);
             System.out.println("x="+x);
