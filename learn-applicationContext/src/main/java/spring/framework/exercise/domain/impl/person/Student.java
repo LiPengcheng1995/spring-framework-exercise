b27e34ff23a895fe25848d1bd162c8cc87bce0fb
lipengcheng3 <lipengcheng3@jd.com>
2020-03-20 17:29:35 +0800

    modify: 验证数据注入，优先级有效

--- learn-applicationContext/src/main/java/spring/framework/exercise/domain/impl/person/Student.java
+++ learn-applicationContext/src/main/java/spring/framework/exercise/domain/impl/person/Student.java
@@ -1,5 +1,6 @@
 package spring.framework.exercise.domain.impl.person;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import spring.framework.exercise.domain.Person;
 
@@ -10,7 +11,10 @@ import spring.framework.exercise.domain.Person;
  * Date: 2019/10/23
  * Time: 20:23
  * Description:
+ * @date 2020-03-20 17:26
+ * @author lipengcheng3
  */
+@Order(3)
 @Component
 public class Student implements Person {
     @Override
--- learn-applicationContext/src/main/java/spring/framework/exercise/domain/impl/person/Teacher.java
+++ learn-applicationContext/src/main/java/spring/framework/exercise/domain/impl/person/Teacher.java
@@ -1,5 +1,6 @@
 package spring.framework.exercise.domain.impl.person;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import spring.framework.exercise.domain.Person;
 
@@ -10,7 +11,11 @@ import spring.framework.exercise.domain.Person;
  * Date: 2019/10/23
  * Time: 20:23
  * Description:
+ * @date 2020-03-20 17:26
+ * @author lipengcheng3
  */
+@Order(2)
+
 @Component
 public class Teacher implements Person {
     @Override
